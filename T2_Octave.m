0dec3b1e9aee8e661cd6815a6e62f0d209253b45
dvd-r16 <3711111370101@ingenieria.usac.edu.gt>
2024-08-31 19:56:56 -0600

    Delete T2_Octave.m

--- T2_Octave.m
+++
@@ -1,3 +0,0 @@
-pkg load database;
-conn = pq_connect (setdbopts ("dbname", "postgres", "host", "localhost", "port", "5432", "user", "postgres", "password", "Lobodefuego01"));
-N = pq_exec_params (conn, "select * from T202010039;")
